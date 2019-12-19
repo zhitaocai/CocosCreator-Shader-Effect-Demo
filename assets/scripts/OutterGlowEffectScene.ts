f383b8edec336ddf636d4f88c5f8298d10de1331
caizhitao <czt.saisam@gmail.com>
2019-12-19 16:25:44 +0800

    完善demo

--- assets/scripts/OutterGlowEffectScene.ts
+++ assets/scripts/OutterGlowEffectScene.ts
@@ -1,41 +1,34 @@
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class MainSceneCtrl extends cc.Component {
-    @property(cc.Sprite)
-    exampleSprite: cc.Sprite = null;
-
-    @property(cc.Label)
-    exampleLabel: cc.Label = null;
+export default class OutterGlowEffectScene extends cc.Component {
+    @property(cc.Node)
+    examplesParentNode: cc.Node = null;
 
     start() {
-        this._updateSpriteOutlineWidth(0);
-        this._updateLabelOutlineWidth(0);
+        this._updateRenderComponentOutterGlowMaterial(0);
     }
 
     onSideCallBack(slider: cc.Slider, customEventData: string) {
-        this._updateSpriteOutlineWidth(slider.progress / 100);
-        this._updateLabelOutlineWidth(slider.progress / 100);
+        this._updateRenderComponentOutterGlowMaterial(slider.progress / 100);
     }
 
     /**
-     * 动态更新描边宽度
+     * 更新渲染组件的材质
      *
      * 1. 获取材质
      * 2. 给材质的自定义 unitform 变量赋值
      * 3. 重新将材质赋值回去
      *
-     * @property outlineSize 描边长度比例[0,1] 比如0.5，那么就是宽*0.5 高*0.5
+     * @param size 描边长度比例[0,1] 比如0.5，那么就是宽*0.5 高*0.5
      */
-    private _updateSpriteOutlineWidth(outlineSize: number) {
-        let spriteMaterial: cc.Material = this.exampleSprite.getMaterial(0);
-        spriteMaterial.setProperty("outlineSize", outlineSize);
-        this.exampleSprite.setMaterial(0, spriteMaterial);
-    }
-
-    private _updateLabelOutlineWidth(outlineSize: number) {
-        let labelMaterial: cc.Material = this.exampleLabel.getMaterial(0);
-        labelMaterial.setProperty("outlineSize", outlineSize);
-        this.exampleLabel.setMaterial(0, labelMaterial);
+    private _updateRenderComponentOutterGlowMaterial(size: number) {
+        this.examplesParentNode.children.forEach(childNode => {
+            childNode.getComponents(cc.RenderComponent).forEach(renderComponent => {
+                let material: cc.Material = renderComponent.getMaterial(0);
+                material.setProperty("outlineSize", size);
+                renderComponent.setMaterial(0, material);
+            });
+        });
     }
 }
